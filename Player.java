87029d16e7cc27a40a3abb0c9c16eb80a19bf755
DankSouls2002 <65518518+DankSouls2002@users.noreply.github.com>
2022-05-20 13:47:23 -0700

    Update Player.java
    Filling in Player with my own code for now, sorry if it's not much better than the placeholder code it replaced!

--- Player.java
+++ Player.java
@@ -1,38 +1,94 @@
 package monsterdungeon;
 import monsterdungeon.Character;
-public class Player extends Character 
-{
+public class Player {
+
+	//By Payton Bayron. More than open to suggestions for additions!
 	
-	Player(int hpMax, int hp, int stamMax, int stam, int manMax, int mana)
+	private String name;
+	
+	private int healthMax;
+	private int manaMax;
+	private int staminaMax;
+	
+	private int healthCurrent;
+	private int manaCurrent;
+	private int staminaCurrent;
+	
+	// ---- Stat Maximum Getters and Setters ----
+	
+	// ---- Health Max ----
+	public int getHealthMax()
 	{
-		this.hpMax = hpMax;
-		this.hp = hp;
-		this.stamMax = stamMax;
-		this.stam = stam;
-		this.manMax = manMax;
-		this.mana = mana;
+		return healthMax;
 	}
 	
-	public void Use(Gem gem)
+	public void setHealthMax(int newHealthMax)
 	{
-		if(gem.bigGem == false)
-		{
-			this.hp += gem.hp;
-			this.stam += gem.stam;
-			this.mana += gem.mana;
-		}
-		if(gem.fullHeal)
-		{
-			this.hp=this.hpMax;
-			this.stam=this.stamMax;
-			this.mana=this.manMax;
-		}
-		else
-		{
-			this.hpMax += gem.hp;
-			this.stamMax += gem.stam;
-			this.manMax += gem.mana;
-		}
+		healthMax = newHealthMax;
+	}
+	// ---- Mana Max ----
+	public int getManaMax()
+	{
+		return manaMax;
 	}
 	
+	public void setManaMax(int newManaMax)
+	{
+		manaMax = newManaMax;
+	}
+	// ---- Stamina Max ----
+	public int getStaminaMax()
+	{
+		return staminaMax;
+	}
+	
+	public void setStaminaMax(int newStaminaMax)
+	{
+		staminaMax = newStaminaMax;
+	}
+	
+	// ---- Stat Current Getters and Setters ----
+	
+	// ---- Health Current ----
+	public int getHealthCurrent()
+	{
+		return healthCurrent;
+	}
+	
+	public void setHealthCurrent(int newHealthCurrent)
+	{
+		healthMax = newHealthCurrent;
+	}
+	// ---- Mana Current ----
+	public int getManaCurrent()
+	{
+		return manaCurrent;
+	}
+	
+	public void setManaCurrent(int newManaCurrent)
+	{
+		manaCurrent = newManaCurrent;
+	}
+	// ---- Stamina Max ----
+	public int getStaminaCurrent()
+	{
+		return staminaCurrent;
+	}
+	
+	public void setStaminaCurrent(int newStaminaCurrent)
+	{
+		staminaMax = newStaminaCurrent;
+	}
+	
+	// ---- Name ----
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String newName)
+	{
+		name = newName;
+	}
 }
