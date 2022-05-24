d76cfd876ddf38017684d80db59c1e9275667d16
YewnAcorn <99105029+YewnAcorn@users.noreply.github.com>
2022-05-24 15:14:36 -0700

    random number generator

--- Gem.java
+++ Gem.java
@@ -6,24 +6,19 @@ public class Gem extends World {
 	int mana;
 	boolean fullHeal;
 	boolean bigGem;
-	Gem(){
 
-	}
 	Gem(Monster monster)
 	{
 		this.hp = monster.hp / 3;
 		this.stam = monster.stam / 7;
 		this.mana = monster.mana / 7;
 	}
-	public int getHP() { return this.hp; }
-	public void setHP(int hp) { this.hp = hp; }
-	public int getStam() {return this.stam; }
-	public void setStam(int stam) { this.stam = stam; }
-	public int getMana() {return this.mana; }
-	public void setMana(int mana) { this.stam = mana; }
-	public boolean getFullHeal() { return this.fullHeal; }
-	public void setFullHeal(boolean isFull) { this.fullHeal = isFull; }
-	public boolean getBigGem() { return this.bigGem; }
-	public void setBigGem(boolean isBig ) {this.bigGem = isBig; }
-	
+	public void bigGem()
+	{
+		this.bigGem = true;
+	}
+	public void fullHeal()
+	{
+		this.fullHeal = true;
+	}
 }
--- Utility.java
+++ Utility.java
@@ -1,12 +1,28 @@
 package monsterdungeon;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-	public class Utility
+
+
+public class Utility extends Game
+{
+	static Menu menu;
+	static Printer printer;
+
+	Utility() {
+		Menu menu = new Menu();
+		Printer printer = new Printer();
+	}
+
+
+	public class Menu
 	{
 		public int BatMenu(String prompt)
 		{
 			Scanner menuScnr = new Scanner(System.in);
-			int choice;
+			int choice = 0;
 			do //start of menu loop
 			{
 				System.out.println("testing: we are in the menu!"); //FIXME
@@ -15,26 +31,17 @@ import java.util.Scanner;
 				choice = menuScnr.nextInt();
 
 
-			} while (choice != 0); //0 to quit out
+			} while (choice == 0);
 			menuScnr.close();
 			return choice;
 		}
 		public int AbilityMenu(String prompt)
 	  {
-			System.out.println("We are in the ability menu");
+			System.out.println("We are in the ability menu"); //FIXME
 			Scanner menuScnr = new Scanner(System.in);
 			int choice;
-			choice = menuScnr.nextInt();
-			return choice;
-	  }
 
-
-
-
-
-
-
-
+		}
 
 	}
 
@@ -53,39 +60,24 @@ import java.util.Scanner;
 		}
 		void PrintStat(Character character)
 		{
-			System.out.println("HP: " + character.gethp());
-			System.out.println("Stamina: " + character.getstam());
-			System.out.println("Mana: " + character.getMan());
+
 		}
 
 	}
 	//we will need a method for generating a random int within a range
-		//done:
-	static int RandNumGen(int min, int max)
+	static int RandNumGen(int floor, int celing)
 	{
-		int random_int = (int)Math.floor(Math.random()*(max-min+1)+min);
-		return random_int;
+
 	}
 
 	static Monster RandMonGen()
 	{
-		int maxHP = RandNumGen(1, 100);
-		int maxStam = RandNumGen(1, 100);
-		int maxMana = RandNumGen(1, 100);
-		Monster newMonster = new Monster(maxHP, maxStam, maxMana);
-		return newMonster;
+
 	}
 
-	static Gem RandGemGen()
+	static Item RandItemGen()
 	{
-		int hp = RandNumGen(1, 100);
-		int stam = RandNumGen(1, 100);
-		int mana = RandNumGen(1, 100);
-		Gem newGem = new Gem();
-		newGem.setHP(hp);
-		newGem.setStam(stam);
-		newGem.setMana(mana);
-		return newGem;
+
 	}
 
 }
