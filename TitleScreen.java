f61d244427232b598d0c7096b925a7e69b8d2b86
YewnAcorn <99105029+YewnAcorn@users.noreply.github.com>
2022-05-28 15:18:19 -0700

    decided we could use a title screen

---
+++ TitleScreen.java
@@ -0,0 +1,96 @@
+public class TitleScreen {
+
+    public static void main(String[] args) throws InterruptedException{
+
+        System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^");
+        System.out.println("||||||||||||||||||||||||||");
+        System.out.println("==========================");
+        System.out.println("\\");
+        Thread.sleep(250);
+        System.out.println(" \\");
+        Thread.sleep(250);
+        System.out.println("  \\");
+        Thread.sleep(250);
+        System.out.println("  /");
+        Thread.sleep(250);
+        System.out.println(" /");
+        Thread.sleep(250);
+        System.out.println("/");
+        Thread.sleep(250);
+        System.out.println("Deep under the earth");
+        System.out.println("\\");
+        Thread.sleep(250);
+        System.out.println(" \\");
+        Thread.sleep(250);
+        System.out.println("  \\");
+        Thread.sleep(250);
+        System.out.println("    \\");
+        Thread.sleep(250);
+        System.out.println("      \\");
+        Thread.sleep(250);
+        System.out.println("      /");
+        Thread.sleep(250);
+        System.out.println("     /");
+        Thread.sleep(250);
+        System.out.println("    /");
+        Thread.sleep(250);
+        System.out.println("   /");
+        Thread.sleep(250);
+        System.out.println("  /");
+        Thread.sleep(250);
+        System.out.println(" /");
+        Thread.sleep(250);
+        System.out.println("\\");
+        Thread.sleep(250);
+        System.out.println(" \\");
+        Thread.sleep(250);
+        System.out.println("Deep underground");
+        System.out.println("     _____  ");
+        System.out.println("    ( ◍  ◍ ) ");
+        System.out.println("     乁ꈍꈍꈍㄏ      ");
+        Thread.sleep(250);
+        System.out.println("     /");
+        Thread.sleep(250);
+        System.out.println("    /");
+        Thread.sleep(250);
+        System.out.println("   /");
+        Thread.sleep(250);
+        System.out.println(" /");
+        Thread.sleep(250);
+        System.out.println("/");
+        Thread.sleep(250);
+        System.out.println("The greatest peril awaits!");
+        System.out.println("\\");
+        Thread.sleep(250);
+        System.out.println(" \\");
+        Thread.sleep(250);
+        System.out.println("  \\");
+
+        Thread.sleep(250);
+        System.out.println("    \\");
+        Thread.sleep(250);
+        System.out.println("      \\");
+        Thread.sleep(250);
+        System.out.println("       in");
+        System.out.println("     /");
+        Thread.sleep(250);
+        System.out.println("    /");
+        Thread.sleep(250);
+        System.out.println("   /");
+        Thread.sleep(250);
+        System.out.println(" /");
+        Thread.sleep(250);
+        System.out.println("/");
+        Thread.sleep(250);
+        System.out.println(“THE”);
+        System.out.println(" ▄▄▄▄▄▄▄ ▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄    ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄    ▄▄▄▄▄▄  ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄   ▄▄ ");
+        System.out.println("█       █      █  █ █  █       █       █  █       █       █  █      ██       █       █  █▄█  █");
+        System.out.println("█       █  ▄   █  █▄█  █    ▄▄▄█  ▄▄▄▄▄█  █   ▄   █    ▄▄▄█  █  ▄    █   ▄   █   ▄   █       █");
+        System.out.println("█     ▄▄█ █▄█  █       █   █▄▄▄█ █▄▄▄▄▄   █  █ █  █   █▄▄▄   █ █ █   █  █ █  █  █ █  █       █");
+        System.out.println("█    █  █      █       █    ▄▄▄█▄▄▄▄▄  █  █  █▄█  █    ▄▄▄█  █ █▄█   █  █▄█  █  █▄█  █       █");
+        System.out.println("█    █▄▄█  ▄   ██     ██   █▄▄▄ ▄▄▄▄▄█ █  █       █   █      █       █       █       █ ██▄██ █");
+        System.out.println("█▄▄▄▄▄▄▄█▄█ █▄▄█ █▄▄▄█ █▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█  █▄▄▄▄▄▄▄█▄▄▄█      █▄▄▄▄▄▄██▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█▄█   █▄█");
+
+
+    }
+}
