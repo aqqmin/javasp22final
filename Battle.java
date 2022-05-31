068a33bace14b34bd7a30c712ce54dad3594e168
aqqmin <69249534+aqqmin@users.noreply.github.com>
2022-05-30 19:32:06 -0700

    overhaul to battle method, in progress
    creating code to work with Ability class data structure I have designed, which will allow for modular deployment of various kinds of abilities by simply altering integer values that are loaded into the class instance's constructor

--- Battle.java
+++ Battle.java
@@ -1,92 +1,151 @@
-package monsterdungeon;
+package dungeonzero;
 
-import monsterdungeon.Character.*;
-
-public class Battle
-{
+public class Battle {
+	
 	Player player;
 	Monster monster;
-	boolean battling = true;
-	World world;
-
+	
+	
 	Battle(Player player, Monster monster)
 	{
 		this.player = player;
 		this.monster = monster;
+		BattleLoop();
+	
 	}
-	public Player BattleTime()
+	
+	protected void BattleLoop()
 	{
-		while (battling)
-		{
-			PlayerTurn();
-			MonsterTurn();
-		}
-		player.Use(monster.gem);
-		return player;
+		
 	}
-	private void MonsterTurn()
+	
+	protected void UsePlayerAbility(Character player, Character monster, Ability ability)
 	{
-		Utility.printer.PrintFile(BatFrameTop.txt);
-		Utility.printer.PrintStat(this.monster);
-		Utility.printer.PrintStat(this.player);
-		Utility.printer.PrintFile(BatFrameBot.txt);
-		int choiceCode = RandNumGen(0,2);
-
-
-		switch (choiceCode) //Monsters controller interface
+		AbilityCalculator(ability, player, monster);			
+	}
+		
+	
+	protected void UseMonsterAbility(Character monster, Character player)
+	{
+		Ability[] monsterAbilities = monster.getAbilities();
+		Ability ability = monsterAbilities[Utility.randGen.Generate(0, monsterAbilities.length)];
+		AbilityCalculator(ability, monster, player);
+		
+		
+	}
+	
+	protected void AbilityCalculator(Ability ability, Character user, Character target)
+	{
+		int resultAmt1;
+		int resultAmt2;
+		
+		//different arithmetic expressions create different stats comparison dynamics, expression are generalized to compare
+		//stat1 and stat2, regardless of what stats are encoded in the ability as stat 1 and stat2
+		
+		switch (ability.getOperatorCode())
 		{
 		case 0:
-			Attack(this.monster, this.player);
-			break;
-
+			
 		case 1:
-			Defend(player);
-			break;
-
-			case 2:
-			MonsterAbility(monster);
-			break;
-		}
-
-		if (this.player.getHp() <= 0 )
-		{
-			GameOver();
-		}
-		if (this.monster.getHp() <= 0)
-		{
-			Victory();
-		}
-	}
-	private void PlayerTurn()
-	{
-		Utility.printer.PrintFile(BatFrameTop.txt);
-		Utility.printer.PrintStat(this.monster);
-		Utility.printer.PrintStat(this.player);
-		Utility.printer.PrintFile(BatFrameBot.txt);
-
-		int choiceCode = Utility.menu.BatMenu("0==[ [1] attack  [2] defend  [3] use ability ==>");
-		//BatMenu prints a prompt and returns an int, which is saved in choiceCode
-
-		case (choiceCode) //player control interface
-		{
-		case 1:
-			Attack(this.player, this.monster);
-			break;
+			
 		case 2:
-			Defend(player);
-			break;
+			
 		case 3:
-			PlayerAbility(player, Utility.menu.AbilityMenu(player));
-			break;
+			
+		case 4:
+		
+		case 5:
+		
+		case 6:
+		
+		case 7:
+			
 		}
-
-		if (this.player.getHp() <= 0 )
+		
+		//this switch carries out the change of the user stat based on calculations above and an integer switch in the ability
+		
+		switch(ability.getResultStat1())
 		{
-			GameOver();
+		case 0:
+			
+		case 1:
+		
+		case 2:
+			
 		}
-		if (this.monster.getHp() <= 0)
+		
+		//this switch carries out the change of the target(or user) stat based on calculations above and an integer switch in the ability
+		switch(ability.getResultStat2())
 		{
-			Victory();
+		case 0:
+			returnVal = this.player.getHp();
+		case 1:
+			returnVal = this.player.getMaxHp();
+		case 2:
+			returnVal = this.player.getMana();
+		case 3:
+			returnVal = this.player.getMaxMana();
+		case 4:
+			returnVal = this.player.getStam();
+		case 5:
+			returnVal = this.player.getMaxStam();
+		case 6:
+			returnVal = this.monster.getHp();
+		case 7:
+			returnVal = this.monster.getMaxHp();
+		case 8:
+			returnVal = this.monster.getMana();
+		case 9:
+			returnVal = this.monster.getMaxMana();
+		case 10:
+			returnVal = this.monster.getStam();
+		case 11:
+			returnVal = this.monster.getMaxStam();
 		}
+		return returnArr;
+		
 	}
-}
+	
+	
+	//returns the value of the stat selected by the stat code, used to parse any instance of a stat selection in an ability
+	//has 12 cases, 1 each for the current and max value of each stat for both monster and player in the current battle
+	//cases are in this order:
+	/*
+	 * 
+	 */
+	protected int StatValue(int statCode)
+	{	
+		int returnVal = 0;
+		switch (statCode)
+		{
+			case 0:
+				returnVal = this.player.getHp();
+			case 1:
+				returnVal = this.player.getMaxHp();
+			case 2:
+				returnVal = this.player.getMana();
+			case 3:
+				returnVal = this.player.getMaxMana();
+			case 4:
+				returnVal = this.player.getStam();
+			case 5:
+				returnVal = this.player.getMaxStam();
+			case 6:
+				returnVal = this.monster.getHp();
+			case 7:
+				returnVal = this.monster.getMaxHp();
+			case 8:
+				returnVal = this.monster.getMana();
+			case 9:
+				returnVal = this.monster.getMaxMana();
+			case 10:
+				returnVal = this.monster.getStam();
+			case 11:
+				returnVal = this.monster.getMaxStam();
+		}
+		return returnVal;
+	}
+		
+		
+		
+	}
